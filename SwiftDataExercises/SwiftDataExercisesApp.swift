6a147a4c6566d2a6fd147d86a2829ffcae8907ae
Patricia M Espert <pmespert@gmail.com>
2026-07-24 04:36:57 +0200

    Initial Commit

---
+++ SwiftDataExercises/SwiftDataExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExercisesApp.swift
+//  SwiftDataExercises
+//
+//  Created by Patricia M Espert on 24/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
