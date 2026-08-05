20af2b9bebb31967aee1ef6a4f3e7376f8038791
Patricia Martinez Espert <patrimespert@outlook.com>
2026-08-05 06:57:40 +0200

    add changes

---
+++ SwiftDataExercisesTests/ViewModel/CharacterViewModel3Tests.swift
@@ -0,0 +1,43 @@
+//
+//  CharacterViewModel3Tests.swift
+//  SwiftDataExercisesTests
+//
+//  Created by Patricia M Espert on 04/08/2026.
+//
+
+import Foundation
+import Testing
+
+@testable import SwiftDataExercises
+
+@MainActor
+struct CharacterViewModel3Tests {
+
+    @Test("Initial loading")
+    func initialLoading() async throws {
+        
+        //Given
+        let useCase = MockLoadAndSortCharactersUseCase3(result: .success(.characterPageTest3))
+        let sut = CharacterViewModel3(loadAndSortCharactersUseCase: useCase)
+        let expectedCharacters = CharacterPage3.characterPageTest3.characters
+        
+        //When
+        
+        let _ = await sut.getCharacters3()
+        
+        //Then
+        switch sut.loadingState {
+            case .loaded(let characters):
+                #expect(characters == expectedCharacters)
+            case .loading:
+                Issue.record("Expected loadingState to be .loaded, got: .loading")
+            case .empty:
+                Issue.record("Expected loadingState to be .loaded, got: .empty")
+            default:
+                Issue.record("Expected loadingState to be .loaded, got: \(sut.loadingState)")
+        }
+        
+        #expect(useCase.callCount == 1)
+        #expect(useCase.receivedPage == 1)
+    }
+}
