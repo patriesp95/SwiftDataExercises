6b312bac5d0fa2be7ed0ef38f12286585c68c131
Patricia Martinez Espert <patrimespert@outlook.com>
2026-08-06 06:39:50 +0200

    add tests del viewmodel

--- SwiftDataExercisesTests/ViewModel/CharacterViewModel3Tests.swift
+++ SwiftDataExercisesTests/ViewModel/CharacterViewModel3Tests.swift
@@ -12,6 +12,26 @@ import Testing
 
 @MainActor
 struct CharacterViewModel3Tests {
+    
+    @Test("Initial state")
+    func initialState() async throws {
+        
+        //Given
+        let useCase = MockLoadAndSortCharactersUseCase3(result: .success(.characterPageTest3))
+        let sut = CharacterViewModel3(loadAndSortCharactersUseCase: useCase)
+        
+        //When
+        
+        //Then
+        switch sut.loadingState {
+            case .idle:
+                #expect(sut.characters3.characters == [])
+                #expect(sut.characters3.nextPage == nil)
+            default:
+                Issue.record("Expected loadingState to be .loaded, got: \(sut.loadingState)")
+        }
+        
+    }
 
     @Test("Initial loading works")
     func initialLoadingWorks() async throws {
